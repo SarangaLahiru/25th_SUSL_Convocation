97f52b2cfb4552aa8c3a6c0d90d58e87c9350ee6
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-08-04 20:03:29 +0530

    v_1

---
+++ convocation/resources/views/welcome.blade.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Convocation Details</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="https://cdn.ayroui.com/1.0/css/bootstrap.min.css"/>
+
+    <!--====== Lineicons CSS ======-->
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet" />
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="https://cdn.ayroui.com/1.0/css/starter.css" />
+</head>
+<body>
+    <!--====== SLIDER ONE PART START ======-->
+    <section class="slider-area slider-one">
+       <div class="bd-example">
+          <div id="carouselOne" class="carousel slide" data-ride="carousel">
+             <ol class="carousel-indicators">
+                <li data-target="#carouselOne" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselOne" data-slide-to="1"></li>
+                <li data-target="#carouselOne" data-slide-to="2"></li>
+             </ol>
+             <div class="carousel-inner">
+                <div class="carousel-item bg_cover active" style="background-image: url(https://cdn.ayroui.com/1.0/images/slider/slider-one/1.jpg);">
+                   <div class="carousel-caption">
+                      <div class="container">
+                         <div class="row justify-content-center">
+                            <div class="col-xl-6 col-lg-7 col-sm-10">
+                               <h2 class="carousel-title">
+                                  Unlimited Friendly & Easy Customisable
+                               </h2>
+                               <p class="text">
+                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
+                               </p>
+                               <ul class="carousel-btn rounded-buttons">
+                                  <li><a class="btn primary-btn rounded-full" href="javascript:void(0)">GET STARTED</a></li>
+                                  <li><a class="btn primary-btn-outline rounded-full" href="javascript:void(0)">DOWNLOAD</a></li>
+                               </ul>
+                            </div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div class="carousel-item bg_cover" style="background-image: url(https://cdn.ayroui.com/1.0/images/slider/slider-one/2.jpg);">
+                   <div class="carousel-caption">
+                      <div class="container">
+                         <div class="row justify-content-center">
+                            <div class="col-xl-6 col-lg-7 col-sm-10">
+                               <h2 class="carousel-title">
+                                  Unlimited Friendly & Easy Customisable
+                               </h2>
+                               <p class="text">
+                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
+                               </p>
+                               <ul class="carousel-btn rounded-buttons">
+                                  <li><a class="btn primary-btn rounded-full" href="javascript:void(0)">GET STARTED</a></li>
+                                  <li><a class="btn primary-btn-outline rounded-full" href="javascript:void(0)">DOWNLOAD</a></li>
+                               </ul>
+                            </div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div class="carousel-item bg_cover" style="background-image: url(https://cdn.ayroui.com/1.0/images/slider/slider-one/3.jpg);">
+                   <div class="carousel-caption">
+                      <div class="container">
+                         <div class="row justify-content-center">
+                            <div class="col-xl-6 col-lg-7 col-sm-10">
+                               <h2 class="carousel-title">
+                                  Unlimited Friendly & Easy Customisable
+                               </h2>
+                               <p class="text">
+                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
+                               </p>
+                               <ul class="carousel-btn rounded-buttons">
+                                  <li><a class="btn primary-btn rounded-full" href="javascript:void(0)">GET STARTED</a></li>
+                                  <li><a class="btn primary-btn-outline rounded-full" href="javascript:void(0)">DOWNLOAD</a></li>
+                               </ul>
+                            </div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+             <a class="carousel-control-prev" href="#carouselOne" role="button" data-slide="prev">
+                <i class="lni lni-chevron-left"></i>
+             </a>
+             <a class="carousel-control-next" href="#carouselOne" role="button" data-slide="next">
+                <i class="lni lni-chevron-right"></i>
+             </a>
+          </div>
+       </div>
+    </section>
+    <!--====== SLIDER ONE PART ENDS ======-->
+
+    <!--====== FEATURE ONE PART START ======-->
+<section class="features-area features-one">
+   <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-lg-6">
+            <div class="section-title text-center">
+               <h3 class="title">25th Convocation </h3>
+               <p class="text">
+                  Register for your convocation ceremony for the respective faculties below.
+               </p>
+            </div>
+            <!-- row -->
+         </div>
+      </div>
+      <!-- row -->
+      <div class="row justify-content-center">
+         <div class="col-lg-3 col-md-7 col-sm-9">
+            <div class="features-style-one text-center">
+               <div class="features-icon">
+                  <i class="lni lni-compass"></i>
+               </div>
+               <div class="features-content">
+                  <h4 class="features-title" >Faculty of Applied Sciences</h4>
+                  <p class="text">
+                     Short description for the ones who look for something new.
+                     Awesome!
+                  </p>
+                  <div class="features-btn rounded-buttons">
+                     <a
+                        class="btn primary-btn-outline rounded-full"
+                        href="javascript:void(0)"
+                        >
+                     KNOW MORE
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <!-- single features -->
+         </div>
+         <div class="col-lg-3 col-md-7 col-sm-9">
+            <div class="features-style-one text-center">
+               <div class="features-icon">
+                  <i class="lni lni-construction"></i>
+               </div>
+               <div class="features-content">
+                  <h4 class="features-title">Faculty of Geomatics</h4>
+                  <p class="text">
+                     Short description for the ones who look for something new.
+                     Awesome!
+                  </p>
+                  <div class="features-btn rounded-buttons">
+                     <a
+                        class="btn primary-btn-outline rounded-full"
+                        href="javascript:void(0)"
+                        >
+                     KNOW MORE
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <!-- single features -->
+         </div>
+         <div class="col-lg-3 col-md-7 col-sm-9">
+            <div class="features-style-one text-center">
+               <div class="features-icon">
+                  <i class="lni lni-cup"></i>
+               </div>
+               <div class="features-content">
+                  <h4 class="features-title">UI & UX Design</h4>
+                  <p class="text">
+                     Short description for the ones who look for something new.
+                     Awesome!
+                  </p>
+                  <div class="features-btn rounded-buttons">
+                     <a
+                        class="btn primary-btn-outline rounded-full"
+                        href="javascript:void(0)"
+                        >
+                     KNOW MORE
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <!-- single features -->
+         </div>
+         <div class="col-lg-3 col-md-7 col-sm-9">
+            <div class="features-style-one text-center">
+               <div class="features-icon">
+                  <i class="lni lni-cup"></i>
+               </div>
+               <div class="features-content">
+                  <h4 class="features-title">UI & UX Design</h4>
+                  <p class="text">
+                     Short description for the ones who look for something new.
+                     Awesome!
+                  </p>
+                  <div class="features-btn rounded-buttons">
+                     <a
+                        class="btn primary-btn-outline rounded-full"
+                        href="javascript:void(0)"
+                        >
+                     KNOW MORE
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <!-- single features -->
+         </div>
+      </div>
+      <!-- row -->
+
+   </div>
+   <!-- container -->
+</section>
+<!--====== FEATURE ONE PART ENDS ======-->
+
+    <!-- Bootstrap and necessary JavaScript libraries -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
